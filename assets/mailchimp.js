2a0ee1fcaebe38012db6de34a12ec76d338abdb0
Maxx Crawford <maxx.crawford@gmail.com>
2020-03-10 17:21:26 -0500

    Moved inline JS to file, styled form, updated text, added privacy policy link

---
+++ assets/mailchimp.js
@@ -0,0 +1,15 @@
+(function ($) {
+	window.fnames = new Array();
+	window.ftypes = new Array();
+	fnames[0] = 'EMAIL';
+	ftypes[0] = 'email';
+	fnames[1] = 'FNAME';
+	ftypes[1] = 'text';
+	fnames[2] = 'LNAME';
+	ftypes[2] = 'text';
+	fnames[3] = 'INTEREST';
+	ftypes[3] = 'text';
+	fnames[4] = 'COMMENT';
+	ftypes[4] = 'textarea';
+}(jQuery));
+var $mcj = jQuery.noConflict(true);
