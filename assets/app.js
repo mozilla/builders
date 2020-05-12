047f13fa7a4cb5744f793281bd039600c7d47e7f
Maxx Crawford <maxx.crawford@gmail.com>
2020-05-11 22:23:27 -0500

    Made FAQ nav sticky

---
+++ assets/app.js
@@ -0,0 +1,15 @@
+(function () {
+  const faqs = document.getElementById("faqs");
+  const sticky = document.getElementById("sticky");
+  const footer = document.getElementById("footer");
+
+  const inview = new Waypoint.Inview({
+    element: footer,
+    enter: function(direction) {
+      sticky.className = "bottom";
+    },
+    exited: function(direction) {
+      sticky.className = "sticky";
+    }
+  });
+})();
