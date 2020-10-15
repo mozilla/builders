5c61727d2a294ff0c1b6f88581c456b8fb375a17
Maxx Crawford <maxx.crawford@gmail.com>
2020-10-15 01:03:25 -0500

    Fixed #31 - Added Launch Party overlay

--- assets/app.js
+++ assets/app.js
@@ -1,15 +1,23 @@
-(function () {
-  // const faqs = document.getElementById("faqs");
-  // const sticky = document.getElementById("sticky");
-  // const footer = document.getElementById("footer");
-  //
-  // const inview = new Waypoint.Inview({
-  //   element: footer,
-  //   enter: function(direction) {
-  //     sticky.className = "bottom";
-  //   },
-  //   exited: function(direction) {
-  //     sticky.className = "sticky";
-  //   }
-  // });
+(function() {
+    'use strict';
+
+    var closeButton = document.querySelector('.js-fti-launch-close');
+    var ftiModal = document.querySelector('.fti-launch');
+
+    function hideModal() {
+      ftiModal.classList.add('hidden');
+      document.body.removeEventListener("mousedown", hideModal, true);
+      document.body.removeEventListener('keydown', keyPressHandler);
+    }
+
+    function keyPressHandler(e) {
+      if (e.key == "Escape") {
+        hideModal();
+      }
+    }
+
+    // Add listeners to close button and esc key
+    closeButton.addEventListener('click', hideModal);
+    document.body.addEventListener('keydown', keyPressHandler);
+
 })();
